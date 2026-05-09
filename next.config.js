012967056a6e86121efb47c9116a7a8c8ccae06e
Tarrah4Tammwe <133949570+Tarrah4Tammwe@users.noreply.github.com>
2026-05-09 18:32:37 +0100

    Fix www redirect: normalize all traffic to non-www domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.tenantrightsuk.info',
+          },
+        ],
+        destination: 'https://tenantrightsuk.info/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
